3d8560c4308ca6a75f8cf994d6f625c8ba319c6c
Roma Sokolkov <rsokolkov@gmail.com>
2019-09-20 14:28:17 +0200

    clang-format changes

--- kalman.cpp
+++ kalman.cpp
@@ -1,14 +1,14 @@
-#include <Eigen/Dense>
 #include "kalman.hpp"
+#include <Eigen/Dense>
 
-Kalman::Kalman():
-  initialized_(false),
-  lastPredTime_(0.),
-  state_(STATE_SIZE),
-  H_(STATE_SIZE, STATE_SIZE),
-  F_(STATE_SIZE, STATE_SIZE),
-  Q_(STATE_SIZE, STATE_SIZE),
-  P_(STATE_SIZE, STATE_SIZE)
+Kalman::Kalman()
+    : initialized_(false),
+      lastPredTime_(0.),
+      state_(STATE_SIZE),
+      H_(STATE_SIZE, STATE_SIZE),
+      F_(STATE_SIZE, STATE_SIZE),
+      Q_(STATE_SIZE, STATE_SIZE),
+      P_(STATE_SIZE, STATE_SIZE)
 {
   reset();
 }
@@ -16,16 +16,20 @@ Kalman::Kalman():
 void Kalman::reset()
 {
   H_.setZero();
-  H_(0,0) = 1.; // observe x
-  H_(1,1) = 1.; // observe y
-  H_(2,2) = 1.; // observe yaw
+  H_(0, 0) = 1.;  // observe x
+  H_(1, 1) = 1.;  // observe y
+  H_(2, 2) = 1.;  // observe yaw
 
   F_.setIdentity();
 
   // Process noise covariance. TODO: Make configurable.
   Q_.setZero();
-  Q_(0,0) = 0.5; Q_(1,1) = 0.5; Q_(2,2) = 0.05; // x,y,yaw
-  Q_(3,3) = 0.1; Q_(4,4) = 0.1; Q_(5,5) = 0.01; // vx,vy,vyaw
+  Q_(0, 0) = 0.5;
+  Q_(1, 1) = 0.5;
+  Q_(2, 2) = 0.05;  // x,y,yaw
+  Q_(3, 3) = 0.1;
+  Q_(4, 4) = 0.1;
+  Q_(5, 5) = 0.01;  // vx,vy,vyaw
 
   state_.setZero();
   P_.setZero();
@@ -35,24 +39,24 @@ void Kalman::reset()
 
 void Kalman::predict(const double timestamp)
 {
-  if (!initialized_)
-  {
+  if (!initialized_) {
     return;
   }
 
   // Update transition matrix (motion model) with time delta.
   double dt = timestamp - lastPredTime_;
-  F_(0,3) = dt; F_(1,4) = dt; F_(2,5) = dt;
+  F_(0, 3) = dt;
+  F_(1, 4) = dt;
+  F_(2, 5) = dt;
 
   // Propagate state and error.
-  state_ = F_* state_;
+  state_ = F_ * state_;
   P_ = F_ * (P_ * F_.transpose()) + Q_;
 }
 
 void Kalman::correct(const Observation& obs)
 {
-  if (!initialized_)
-  {
+  if (!initialized_) {
     // Use first observation as ground truth.
     state_ = obs.observation;
     lastPredTime_ = obs.timestamp;
@@ -62,7 +66,7 @@ void Kalman::correct(const Observation& obs)
 
   // (1) Compute kalman gain. K = (PH') / (HPH' + R)
   Eigen::MatrixXd pht = P_ * H_.transpose();
-  Eigen::MatrixXd hphrInv  = (H_ * pht + obs.covariance).inverse();
+  Eigen::MatrixXd hphrInv = (H_ * pht + obs.covariance).inverse();
   Eigen::MatrixXd K = pht * hphrInv;
 
   Eigen::VectorXd innovation = obs.observation - H_ * state_;
--- kalman.hpp
+++ kalman.hpp
@@ -3,31 +3,28 @@
 
 const int STATE_SIZE = 6;
 
-struct Observation
-{
-  Eigen::Matrix<double,STATE_SIZE,1> observation;
-  Eigen::Matrix<double,STATE_SIZE,STATE_SIZE> covariance;
+struct Observation {
+  Eigen::Matrix<double, STATE_SIZE, 1> observation;
+  Eigen::Matrix<double, STATE_SIZE, STATE_SIZE> covariance;
   double timestamp;
 };
 
-class Kalman
-{
-  public:
-    Kalman();
-    ~Kalman() {};
-    const Eigen::MatrixXd& covariance() const { return P_; };
-    const Eigen::VectorXd& state() const { return state_; };
+class Kalman {
+ public:
+  Kalman();
+  const Eigen::MatrixXd& covariance() const { return P_; };
+  const Eigen::VectorXd& state() const { return state_; };
 
-    void predict(const double timestamp);
-    void correct(const Observation& obs);
-    void reset();
+  void predict(const double timestamp);
+  void correct(const Observation& obs);
+  void reset();
 
-  private:
-    bool initialized_;
-    double lastPredTime_;
-    Eigen::MatrixXd H_; // Observation matrix
-    Eigen::MatrixXd F_; // State transition matrix (system dynamics)
-    Eigen::MatrixXd Q_; // Process noise covariance matrix
-    Eigen::VectorXd state_; // Estimated state vector
-    Eigen::MatrixXd P_; // Estimated error covariance matrix
+ private:
+  bool initialized_;
+  double lastPredTime_;
+  Eigen::MatrixXd H_;      // Observation matrix
+  Eigen::MatrixXd F_;      // State transition matrix (system dynamics)
+  Eigen::MatrixXd Q_;      // Process noise covariance matrix
+  Eigen::VectorXd state_;  // Estimated state vector
+  Eigen::MatrixXd P_;      // Estimated error covariance matrix
 };
