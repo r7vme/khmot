57e1f87e8d27ff10d62267f1d4fcf3965a73af12
Roma Sokolkov <rsokolkov@gmail.com>
2019-10-18 03:10:53 +0200

    WIP: Add dimentions estimation

--- khmot/src/ros_tracker.cpp
+++ khmot/src/ros_tracker.cpp
@@ -32,6 +32,9 @@ void RosTracker::obsCallback(const khmot_msgs::ObservationsArray& msg)
     obsArr.back().state(StateMemberX) = msg.observations[n].x;
     obsArr.back().state(StateMemberY) = msg.observations[n].y;
     obsArr.back().state(StateMemberYaw) = msg.observations[n].yaw;
+    obsArr.back().state(StateMemberH) = msg.observations[n].height;
+    obsArr.back().state(StateMemberW) = msg.observations[n].width;
+    obsArr.back().state(StateMemberL) = msg.observations[n].length;
     obsArr.back().covariance.setZero();
     for (int i = 0; i < OBSERVATION_SIZE; ++i) {
       for (int j = 0; j < OBSERVATION_SIZE; ++j) {
@@ -51,9 +54,11 @@ void RosTracker::obsCallback(const khmot_msgs::ObservationsArray& msg)
     tracksMsg->tracks.back().x = track->KF.state()(StateMemberX);
     tracksMsg->tracks.back().y = track->KF.state()(StateMemberY);
     tracksMsg->tracks.back().yaw = track->KF.state()(StateMemberYaw);
+    tracksMsg->tracks.back().height = track->KF.state()(StateMemberH);
+    tracksMsg->tracks.back().width = track->KF.state()(StateMemberW);
+    tracksMsg->tracks.back().length = track->KF.state()(StateMemberL);
     tracksMsg->tracks.back().vx = track->KF.state()(StateMemberVx);
     tracksMsg->tracks.back().vy = track->KF.state()(StateMemberVy);
-    tracksMsg->tracks.back().vyaw = track->KF.state()(StateMemberVyaw);
     tracksMsg->tracks.back().vyaw = track->KF.state()(StateMemberVyaw);
     for (int i = 0; i < STATE_SIZE; ++i) {
       for (int j = 0; j < STATE_SIZE; ++j) {
