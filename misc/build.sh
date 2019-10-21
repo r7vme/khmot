d42bfa0383f9b3f4784a7e83322397e70fb91859
Roma Sokolkov <rsokolkov@gmail.com>
2019-10-21 19:31:33 +0200

    Enable clang-tidy

--- misc/build.sh
+++ misc/build.sh
@@ -4,9 +4,8 @@ set -e
 [ ! -d build ] && mkdir build
 cd build
 
-export CC=clang-8
-export CXX=clang++-8
-#cmake "-DCMAKE_CXX_CLANG_TIDY=/usr/sbin/clang-tidy;-checks=*" ..
+export CC=clang
+export CXX=clang++
 cmake -DDISABLE_ROS=TRUE ../khmot
 make
 make test
