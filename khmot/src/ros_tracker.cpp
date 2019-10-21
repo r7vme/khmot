50ea070965c1597a1cb4d72d5bcd645da5bbc668
Roma Sokolkov <rsokolkov@gmail.com>
2019-10-21 12:17:52 +0200

    Transform boxes into static frame before tracker

--- khmot/src/ros_tracker.cpp
+++ khmot/src/ros_tracker.cpp
@@ -5,14 +5,18 @@
 namespace khmot {
 
 RosTracker::RosTracker(ros::NodeHandle nh, ros::NodeHandle priv_nh)
-    : frameId_(""), tracker_()
+    : staticFrameId_(""),
+      tfListener_(tfBuffer_),
+      tfTimeout_(ros::Duration(0)),
+      tracker_()
 {
   int publishFreq(0);
   double dimsFilterAlpha(0.0);
   double mahalanobisThresh(0.0);
   double trackTimeout(0.0);
 
-  priv_nh.param<string>("frame_id", frameId_, "camera");
+  // Set to odom if available
+  priv_nh.param<string>("static_frame_id", staticFrameId_, "map");
   priv_nh.param<int>("publish_freq", publishFreq, 10);
   priv_nh.param<double>("dims_filter_alpha", dimsFilterAlpha, 0.1);
   priv_nh.param<double>("mahalanobis_thresh", mahalanobisThresh, 3.0);
@@ -30,25 +34,43 @@ RosTracker::RosTracker(ros::NodeHandle nh, ros::NodeHandle priv_nh)
 }
 
 void RosTracker::obsCallback(
-    const khmot_msgs::BoundingBoxWithCovarianceArray& msg)
+    const khmot_msgs::BoundingBoxWithCovarianceArray &msg)
 {
   vector<Observation> obsArr{};
-  double timestamp = msg.header.stamp.toSec();
+  string frameId = msg.header.frame_id;
+  auto timestampROS = msg.header.stamp;
+  double timestamp = timestampROS.toSec();
+
+  // lookup transform staticFrame->sensor
+  tf2::Transform sensorInStaticTF;
+  if (!lookupTransformSafe(tfBuffer_, staticFrameId_, frameId, timestampROS,
+                           ros::Duration(0), sensorInStaticTF)) {
+    return;
+  }
 
   int obsNum = msg.boxes.size();
   for (int n = 0; n < obsNum; ++n) {
-    // fetch yaw from quaternion
     tf2::Quaternion quat;
+    tf2::Vector3 pose;
     tf2::fromMsg(msg.boxes[n].pose.pose.orientation, quat);
-    double yaw = tf2::getYaw(quat);
+    tf2::fromMsg(msg.boxes[n].pose.pose.position, pose);
+    pose[2] = 0.0;  // XXX: set Z to 0. We are operating in 2D.
+    tf2::Transform objInSensorTF(quat, pose);
 
+    tf2::Transform objInStaticTF;
+    objInStaticTF.mult(sensorInStaticTF, objInSensorTF);
+
+    // fill Observation object
     obsArr.emplace_back();
+    obsArr.back().dims.h = msg.boxes[n].dimensions.x;
+    obsArr.back().dims.w = msg.boxes[n].dimensions.y;
+    obsArr.back().dims.l = msg.boxes[n].dimensions.z;
+
     obsArr.back().kalmanObs.timestamp = timestamp;
-    obsArr.back().kalmanObs.state(StateMemberX) =
-        msg.boxes[n].pose.pose.position.x;
-    obsArr.back().kalmanObs.state(StateMemberY) =
-        msg.boxes[n].pose.pose.position.y;
-    obsArr.back().kalmanObs.state(StateMemberYaw) = yaw;
+    obsArr.back().kalmanObs.state(StateMemberX) = objInStaticTF.getOrigin()[0];
+    obsArr.back().kalmanObs.state(StateMemberY) = objInStaticTF.getOrigin()[1];
+    obsArr.back().kalmanObs.state(StateMemberYaw) =
+        tf2::getYaw(objInStaticTF.getRotation());
     for (int i = 0; i < OBSERVATION_SIZE; ++i) {
       for (int j = 0; j < OBSERVATION_SIZE; ++j) {
         obsArr.back().kalmanObs.covariance(i, j) =
@@ -60,20 +82,23 @@ void RosTracker::obsCallback(
   tracker_.update(obsArr, timestamp);
 }
 
-void RosTracker::publishTracks(const ros::TimerEvent&)
+void RosTracker::publishTracks(const ros::TimerEvent &)
 {
   ros::Time timeNow = ros::Time::now();
 
   // predict position (Kalman)
-  for (const auto& track : tracker_.tracks()) {
+  for (const auto &track : tracker_.tracks()) {
     track->KF.predict(timeNow.toSec());
   }
 
-  jsk_recognition_msgs::BoundingBoxArray bboxMsgArr;
+  jsk_recognition_msgs::BoundingBoxArray bboxArrMsg;
+  bboxArrMsg.header.stamp = timeNow;
+  bboxArrMsg.header.frame_id = staticFrameId_;
+
   jsk_recognition_msgs::BoundingBox bboxMsg;
   bboxMsg.header.stamp = timeNow;
-  bboxMsg.header.frame_id = frameId_;
-  for (const auto& track : tracker_.tracks()) {
+  bboxMsg.header.frame_id = staticFrameId_;
+  for (const auto &track : tracker_.tracks()) {
     bboxMsg.dimensions.x = track->dims.l;
     bboxMsg.dimensions.y = track->dims.w;
     bboxMsg.dimensions.z = track->dims.h;
@@ -85,15 +110,66 @@ void RosTracker::publishTracks(const ros::TimerEvent&)
     bboxMsg.pose.orientation.w = q[3];
     bboxMsg.pose.position.x = track->KF.state()(StateMemberX);
     bboxMsg.pose.position.y = track->KF.state()(StateMemberY);
-    bboxMsg.pose.position.z = track->dims.h / 2;  // bottom on the ground
+    bboxMsg.pose.position.z = track->dims.h / 2;  // XXX: bottom on the ground
     bboxMsg.value = 0;
-    bboxMsgArr.boxes.push_back(bboxMsg);
+    bboxArrMsg.boxes.push_back(bboxMsg);
   }
 
-  if (ros::ok())
-  {
-    boxesPub_.publish(bboxMsgArr);
+  if (ros::ok()) {
+    boxesPub_.publish(bboxArrMsg);
   }
+}
+
+// Function mostly borrowed from robot_localization (BSD)
+// https://github.com/cra-ros-pkg/robot_localization/blob/3bd285e6fc400447086490e833a6f0181284b4d8/LICENSE
+bool lookupTransformSafe(const tf2_ros::Buffer &buffer,
+                         const string &targetFrame, const string &sourceFrame,
+                         const ros::Time &time, const ros::Duration &timeout,
+                         tf2::Transform &targetFrameTrans)
+{
+  bool retVal = true;
+
+  // First try to transform the data at the requested time
+  try {
+    tf2::fromMsg(buffer.lookupTransform(targetFrame, sourceFrame, time, timeout)
+                     .transform,
+                 targetFrameTrans);
+  }
+  catch (tf2::TransformException &ex) {
+    // The issue might be that the transforms that are available are not close
+    // enough temporally to be used. In that case, just use the latest available
+    // transform and warn the user.
+    try {
+      tf2::fromMsg(
+          buffer.lookupTransform(targetFrame, sourceFrame, ros::Time(0))
+              .transform,
+          targetFrameTrans);
+
+      ROS_WARN_STREAM_THROTTLE(
+          2.0, "Transform from " << sourceFrame << " to " << targetFrame
+                                 << " was unavailable for the time "
+                                    "requested. Using latest instead.\n");
+    }
+    catch (tf2::TransformException &ex) {
+      ROS_WARN_STREAM_THROTTLE(2.0, "Could not obtain transform from "
+                                        << sourceFrame << " to " << targetFrame
+                                        << ". Error was " << ex.what() << "\n");
+
+      retVal = false;
+    }
+  }
+
+  // Transforming from a frame id to itself can fail when the tf tree isn't
+  // being broadcast (e.g., for some bag files). This is the only failure that
+  // would throw an exception, so check for this situation before giving up.
+  if (!retVal) {
+    if (targetFrame == sourceFrame) {
+      targetFrameTrans.setIdentity();
+      retVal = true;
+    }
+  }
+
+  return retVal;
 }
 
 }  // namespace khmot
--- khmot/src/ros_tracker.hpp
+++ khmot/src/ros_tracker.hpp
@@ -8,12 +8,18 @@
 #include <ros/ros.h>
 #include <tf2/utils.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_ros/transform_listener.h>
 
 #include <string>
 
 using namespace std;
 
 namespace khmot {
+
+bool lookupTransformSafe(const tf2_ros::Buffer &buffer,
+                         const string &sourceFrame, const string &targetFrame,
+                         const ros::Time &time, const ros::Duration &timeout,
+                         tf2::Transform &targetFrameTrans);
 
 // Subscribes to bboxes and publishes
 class RosTracker {
@@ -29,11 +35,14 @@ class RosTracker {
   ros::Timer pubTimer_;
 
   // Functions.
-  void obsCallback(const khmot_msgs::BoundingBoxWithCovarianceArray& msg);
-  void publishTracks(const ros::TimerEvent&);
+  void obsCallback(const khmot_msgs::BoundingBoxWithCovarianceArray &msg);
+  void publishTracks(const ros::TimerEvent &);
 
+  string staticFrameId_;
   Tracker tracker_;
-  string frameId_;
+  tf2_ros::Buffer tfBuffer_;
+  tf2_ros::TransformListener tfListener_;
+  ros::Duration tfTimeout_;
 };
 
 }  // namespace khmot
